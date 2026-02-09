4472a7dce7a84550661039364f53481ca60355ff
mstne03 <mserraitorne@gmail.com>
2026-02-10 00:43:37 +0100

    fix: include new reviews and erase old ones

--- app/_lib/servicesData.ts
+++ app/_lib/servicesData.ts
@@ -58,7 +58,9 @@ export const servicesData: ServiceSectionType[] = [
                 `L’Art estimula les teves capacitats expressives i creatives. La seva 
                 dimensió dinàmica i simbòlica afaforeix el desbloqueig d’emocions i 
                 pensaments, alhora que obre noves maneres de poder mirar i 
-                mirar-te. L'Artteràpia desplega un espai d'acompanyament terapèutic on els 
+                mirar-te.
+
+                L'Artteràpia desplega un espai d'acompanyament terapèutic on els 
                 llenguatges i processos artístics t’ajuden a trobar les estratègies i 
                 recursos propis per elaborar i donar resposta a moments vitals que 
                 puguis travessar.`,
@@ -76,18 +78,63 @@ export const servicesData: ServiceSectionType[] = [
         bgColor: 'bg-lilac',
         contentKey: 'artterapia',
         icon: ArtterapiaIcon.src,
-        disclaimer: `
-            Per fer aquestes sessions no és necessari que siguis habilidós ni tenir formació artística. Els serveis 
-            d’artteràpia no són sanitaris ni substitueixen tractament mèdics. L’acceptació és voluntària i responsable.`,
+        disclaimer: [
+            `Per fer aquestes sessions no és necessari que siguis habilidós ni tenir formació artística.`,
+            `Els serveis d'artteràpia no són sanitaris ni substitueixen tractament mèdics. L'acceptació és voluntària i responsable.`
+        ],
         reviews: [
+           /* {
+                review: `
+                    Mai hauria imaginat el poder que pot
+                    tenir l’ expressió artística en la 
+                    vida personal, per canalitzar i prendre 
+                    consciència dels efectes que poden tenir 
+                    en una persona les experiències viscudes. 
+                    Gràcies al Taller i a activitats biogràfiques 
+                    com el collage, he vist que això és possible: 
+                    et coneixes a tu mateix, revius i reformules 
+                    fets que han marcat la teva vida i t’adones 
+                    d’allò que ja no pot canviar, però també de 
+                    tot el que sí pots modificar. A més a més, 
+                    el caliu que es genera en el grup i 
+                    l’ acompanyament de la professora fan que 
+                    tot plegat es converteixi en una experiència 
+                    única on expressar lliurement idees i emocions 
+                    a través de l’art.`,
+                author: `M`,
+            },*/
             {
                 review: `
-                    L’Artteràpia ens ha ajudat com a 
-                    família a expressar-nos més i 
-                    millor emocionalment i ens ha 
-                    permès crear noves dinàmiques 
-                    familiars.`,
-                author: `Mare de dos fills adolescents`,
+                    Éstic molt contenta. M'has ajudat molt. Va ser una experiència inoblidable. 
+                    Esto lo dice una niña de 11 años... importante edad de la reseña. Con las clases 
+                    de arteterapia, mi hijo consiguió entender lo que sentía y aprendió a expresarlo, 
+                    sin palabras con sus "pequeñas obras". Míriam consiguió entrar en su mundo... y 
+                    abrir puertas... Estaremos eternamente agradecidos...`,
+                author: `Mare d'una nena d'11 anys`,
+            },
+            {
+                review: `
+                    Comencé mis vacaciones escolares con ocho sesiones de arteterapia, llenas de luz 
+                    y diversión. En todas ellas, estuve en un ambiente relajado y con una arteterapeuta 
+                    cómplice, que me ayudó a mostrar y analizar mis sentimientos. Cada sesión se 
+                    convirtió en un refugio en el que podía expresarme libremente, sin miedo a ser 
+                    juzgada, simplemente me dejé llevar por mi mente y mi corazón. El arte y la pintura 
+                    me guiaron a un mayor autoconocimiento y me hicieron sentir cada vez más liberada 
+                    de mis preocupaciones interiores.`,
+                author: `Jove de 15 anys`,
+            },
+            {
+                review: `
+                    Va ser un moment especial, una motivació que creixia setmana rera setmana. Ell 
+                    tenia l'opció de poder crear elements fantàstics amb els que s'identificava 
+                    emocionalment, descobrint fortaleses i identificant frustracions. La Míriam 
+                    l'acompanyava en aquest procés i, de mica en mica, les converses amb ella, aquestes 
+                    estones, es van convertir en imprescindibles, en un motor que generava alegria i 
+                    que ell esperava amb ganes. El final va arribar sense ganes d'acabar, però el procés 
+                    viscut li havia donat eines per expressar els seus neguits i per fer servir les 
+                    emocions per crear i per descobrir una mena de fortalesa personal que l'ajudi a 
+                    viure les experiències de vida.`,
+                author: `Mare d'un adolescent de 12 anys`,
             },
             {
                 review: `
@@ -131,15 +178,13 @@ export const servicesData: ServiceSectionType[] = [
                     icon: GestoIcon.src,
                 },
                 {
-                    desc: `Espai en família: quan és necessari fer un treball compartit.`,
-                    icon: ArbolIcon.src,
-                },
-                {
                     desc: `Monogràfics amb grup reduït per abordar temes concrets.`,
                     icon: CreatividadIcon.src,
                 },
                 {
-                    desc: `Sessions externes per a centres educatius, ajuntaments, entitats socials i col·lectius d' empresa.`,
+                    desc: `Sessions externes per a centres educatius, ajuntaments, 
+                            hospitals, residències, centres culturals, col.lectius 
+                            d’ empresa i entitats socials.`,
                     icon: PinturasIcon.src,
                 }
             ]
@@ -189,7 +234,6 @@ export const servicesData: ServiceSectionType[] = [
                 L'art és un viatge cap endins... Un viatge cap enfora... \n 
                 És en aquest vaivé que ens posa en moviment i pot ser transformador
             `,
-            layout: `text-only-not-heading`,
             content: [
                 `Les nostres eines seran els pinzells, els acrílics, els papers de petit i gran format, les teles, els olis i els 
                 pigments, les aquarel.les, els carbonets i les sanguines, l’argila, els esmalts i els òxids, les fustes, les 
@@ -199,8 +243,9 @@ export const servicesData: ServiceSectionType[] = [
                 `El treball plàstic és sobretot un treball individual, però també hi hauran moltes propostes
                 col·lectives que s’aniran desplegant al llarg del curs. El grup serà important pel clima de treball i el 
                 vincle amb ell ajudarà a sostenir tot allò que vagi sorgint, així com també serà clau per compartir si 
-                es fa necessari i tancar la sessió. `,
-                `No buscarem una manera “correcta” de pintar, ni donarem valor a una idea
+                es fa necessari i tancar la sessió.
+                
+                No buscarem una manera "correcta" de pintar, ni donarem valor a una idea
                 predeterminada de l’estètica. Partirem del que som i d’ allò que ens configura, fent valer la nostra 
                 mirada de les coses des de les eines que ens proporciona l’Art, tot afavorint un treball personal.`,
             ],
@@ -280,8 +325,10 @@ export const servicesData: ServiceSectionType[] = [
             ],
             images: [],
         },
-        disclaimer: `Per fer aquestes sessions no és necessari que siguis habilidós ni tenir formació artística. Els serveis 
-                    d’artteràpia no són sanitaris ni substitueixen tractament mèdics. L’acceptació és voluntària i responsable.`,
+        disclaimer: [
+            `Per fer aquestes sessions no és necessari que siguis habilidós ni tenir formació artística.`,
+            `Els serveis d'artteràpia no són sanitaris ni substitueixen tractament mèdics. L'acceptació és voluntària i responsable.`
+        ],
         bgColor: 'bg-lilac',
         contentKey: 'artperdins',
         icon: ArtperdinsIcon.src
@@ -307,28 +354,30 @@ export const servicesData: ServiceSectionType[] = [
         questions: [],
         moreContent: {
             buttonText: `Formació per al professorat`,
-            layout: 'text-only',
             content: [
-                `L’objectiu d’aquest curs de formació i assessorament per a 
-                professors d’infantil i primària, és la de repensar els objectius 
-                propis en l’àrea d’Expressió Plàstica, així com la seva metodologia.`, 
-                `Per iniciar aquest procés de replantejament és molt important començar 
+                `L'objectiu d'aquest curs de formació i assessorament per a 
+                professors d'infantil i primària, és la de repensar els objectius 
+                propis en l'àrea d'Expressió Plàstica, així com la seva metodologia.
+                
+                Per iniciar aquest procés de replantejament és molt important començar 
                 atenent quin tipus de relació té el professorat amb l’Art, què es considera 
                 que ho és i què no i com es tradueix en les seves propostes de treball 
                 de cara als alumnes i en la valoració que en fan del resultat.`,
                 `Per tal que la Plàstica tingui contingut en l’àmbit educatiu, serà important 
                 que la proposta plantejada als infants la possibilitat de Significar, és a dir, 
                 que pugui ser suficientment àmplia perquè l’alumne pugui investigar i dotar-la 
-                de sentit personal`,
-                `Amb la finalitat que aquest Significar tingui un sentit ampli, la proposta formativa 
+                de sentit personal.
+                
+                Amb la finalitat que aquest Significar tingui un sentit ampli, la proposta formativa 
                 tindrà un caràcter transversal, entenent la importància de la multiplicitat de 
                 llenguatges artístics (poesia, música, dansa…) i com aquesta amplitud de llenguatges 
-                ens permet estirar el fil de les propostes i la integració i elaboració d’aprenentatges.`,
-                `Es treballarà sobre paraules clau: la Proposta, la Implicació, el Procés de realització i 
+                ens permet estirar el fil de les propostes i la integració i elaboració d'aprenentatges.
+                
+                Es treballarà sobre paraules clau: la Proposta, la Implicació, el Procés de realització i 
                 de les diferències entre Creació i Expressió.`,
             ],
         },
-        disclaimer: ``,
+        disclaimer: [],
         rest: {
             title: "APRENDRE A MIRAR UN QUADRE",
             icon: PaletaIcon.src,
