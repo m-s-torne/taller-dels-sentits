cb2bed8aa987fe3c57642772ffc82fa6fd05fe7f
mstne03 <mserraitorne@gmail.com>
2026-02-10 00:40:31 +0100

    fix: apply new more content section prop structure

--- app/(serveis)/artperdins/page.tsx
+++ app/(serveis)/artperdins/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 import { servicesData } from '@/app/_lib/servicesData';
 import headerImg from './assets/images/taller_dels_sentits_capçalera_artperdins.jpg';
+import angel1 from '../artterapia/assets/images/angel_1.jpg';
+import angel2 from '../artterapia/assets/images/angel_2.jpg';
 import { 
     HeroSection,
     DisclaimerSection, 
@@ -47,7 +49,13 @@ export default function Artperdins() {
 
                 <ImageCarousel images={carouselImages2} />
 
-                <MoreContentSection moreContent={service.moreContent} />
+                <MoreContentSection 
+                    moreContent={service.moreContent}
+                    images={[
+                        { src: angel1.src, alt: 'Àngel 1' },
+                        { src: angel2.src, alt: 'Àngel 2' }
+                    ]}
+                />
 
                 <ReviewsSection reviews={service.reviews!}/>
                 
--- app/(serveis)/centres/page.tsx
+++ app/(serveis)/centres/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 import { servicesData } from '@/app/_lib/servicesData';
 import headerImg from './assets/images/taller_dels_sentits_capçalera_centres.jpg';
+import angel1 from '../artterapia/assets/images/angel_1.jpg';
+import angel2 from '../artterapia/assets/images/angel_2.jpg';
 import { HeroSection, LongDescription, MoreContentSection, StaticQuote } from '@/app/(serveis)/components';
 import { RestCentres } from './components/RestCentres';
 import { useScrollToTop } from '@/app/_hooks/useScrollToTop';
@@ -27,7 +29,13 @@ export default function Centres() {
 
                 <LongDescription longDescription={service.longDescription} />
 
-                <MoreContentSection moreContent={service.moreContent}/>
+                <MoreContentSection 
+                    moreContent={service.moreContent}
+                    images={[
+                        { src: angel1.src, alt: 'Àngel 1' },
+                        { src: angel2.src, alt: 'Àngel 2' }
+                    ]}
+                />
 
                 <RestCentres rest={service.rest}/>
             </div>
