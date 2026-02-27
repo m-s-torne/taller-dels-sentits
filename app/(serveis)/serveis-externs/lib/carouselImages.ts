d2dbba5cf65941c632483105a66db2845931e63c
mstne03 <mserraitorne@gmail.com>
2026-02-28 00:16:21 +0100

    feat: create serveis externs image carousel with new images

---
+++ app/(serveis)/serveis-externs/lib/carouselImages.ts
@@ -0,0 +1,9 @@
+import image1 from '../assets/images/1.jpeg';
+import image2 from '../assets/images/2.jpg';
+import image3 from '../assets/images/3.jpg';
+
+export const carouselImages = [
+    { src: image1.src, alt: 'Serveis externs 1' },
+    { src: image2.src, alt: 'Serveis externs 2' },
+    { src: image3.src, alt: 'Serveis externs 3' },
+];
