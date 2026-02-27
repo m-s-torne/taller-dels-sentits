67a1016ede7ff779f5b2bb56a72f103b508774cc
mstne03 <mserraitorne@gmail.com>
2026-02-27 17:52:28 +0100

    fix: fix heading spacing for artperdins rest section

--- app/(serveis)/artperdins/components/RestArtperdins.tsx
+++ app/(serveis)/artperdins/components/RestArtperdins.tsx
@@ -19,7 +19,7 @@ export const RestArtperDins = ({ rest }: RestArtperDinsProps) => {
             className="py-10"
         >
             <div className="text-center space-y-7">
-                <SectionHeading headingLevel="h2" title={rest.title} className="sm:text-5xl! lg:text-5xl! mb-0!" />
+                <SectionHeading headingLevel="h2" title={rest.title} className="sm:text-5xl! lg:text-5xl!" />
 
                 {rest.content.map((p, i) => {
                     return <p key={i} className="text-left text-sm sm:text-base lg:text-lg ml-6 sm:ml-0">{p}</p>
